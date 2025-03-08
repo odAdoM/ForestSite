b93480d3cf13910d79cc018af78d6def5dc20108
Marta <polanskamarta.com>
2025-03-08 01:26:29 +0100

    offers page (mobile&desktop) done

--- src/js/offers.js
+++ src/js/offers.js
@@ -5,21 +5,39 @@ import * as cc from './add/console-colors.min.js';
 import * as navi from './navi.min.js';
 import * as offer from './offer.min.js';
 
-const tdIcons = document.querySelectorAll('.offer-table td span');
-const allAddIcons = Array.from(tdIcons).filter((i) => i.classList.contains('i_add'));
+const tdIconsMobile = document.querySelectorAll('.offers-box--mobile .offer-table td span');
+const tdIconsDesktop = document.querySelectorAll('.offers-box--desktop .offer-table td span');
+const allAddIconsMobile = Array.from(tdIconsMobile).filter((i) => i.classList.contains('i_add'));
+const allAddIconsDesktop = Array.from(tdIconsDesktop).filter((i) => i.classList.contains('i_add'));
+let allAddIcons = allAddIconsMobile;
 let addIconsTO = 0;
 
 //====================================================================================
 
 const setIcons = () => {
-	if (tdIcons && tdIcons.length > 0) {
-		tdIcons.forEach((ico) => {
+	// mobile ver
+
+	if (tdIconsMobile && tdIconsMobile.length > 0) {
+		tdIconsMobile.forEach((ico) => {
 			offer.setRightIcon(ico);
 		});
 	}
+	if (allAddIconsMobile && allAddIconsMobile.length > 0) {
+		allAddIconsMobile.forEach((ico) => {
+			ico.classList.add('plus-off-animation');
+			ico.style.animation = 'none'; //stop at loading page
+		});
+	}
 
-	if (allAddIcons && allAddIcons.length > 0) {
-		allAddIcons.forEach((ico) => {
+	// desktop ver
+
+	if (tdIconsDesktop && tdIconsDesktop.length > 0) {
+		tdIconsDesktop.forEach((ico) => {
+			offer.setRightIcon(ico);
+		});
+	}
+	if (allAddIconsDesktop && allAddIconsDesktop.length > 0) {
+		allAddIconsDesktop.forEach((ico) => {
 			ico.classList.add('plus-off-animation');
 			ico.style.animation = 'none'; //stop at loading page
 		});
@@ -27,14 +45,14 @@ const setIcons = () => {
 };
 
 const setAddIconsAnimation = (delay) => {
-	//clearTimeout(addIconsTO);
+	clearTimeout(addIconsTO);
 
 	addIconsTO = setTimeout(function () {
 		resetAddIcons();
 		const getThreeNumbers = helper.getRandomFigures(3, 0, allAddIcons.length - 1);
 
 		setTimeout(function () {
-			console.log('%c start anim', cc.bgc.orange);
+			//console.log('%c start anim', cc.bgc.orange);
 			allAddIcons.forEach((ico) => {
 				ico.style.animation = ''; // return to animation from class, at start
 			});
@@ -52,31 +70,33 @@ const setAddIconsAnimation = (delay) => {
 };
 
 const resetAddIcons = () => {
-	console.log('%c clear', cc.colors.green);
+	//console.log('%c clear', cc.colors.green);
 	allAddIcons.forEach((ico) => {
-		//ico.style.animationPlayState = 'paused';
-		//ico.classList.remove('plus-off-animation');
 		ico.style.animation = 'none';
 
 		const ai = ico.parentNode.querySelector('span.new-plus-icon');
 		if (ai != null) {
-			//console.log(ai);
-			//ico.parentNode.removeChild(ai);
 			ai.remove();
 		}
 	});
-
-	// try {
-
-	// console.log(i.classList.contains('new-plus-icon'));
-	// const addedIcon = i.parentNode.querySelector('.new-plus-icon');
-	// console.log(addedIcon);
-	// } catch (error) {
-	// console.log("error by removing dynamic icon at offer's table");
-	// }
 };
+
+//--------
+
+const mediaQuery = window.matchMedia('(min-width: 840px)');
+function mobileViewChangeHandler(e) {
+	let isMobileView;
+	if (e === null) {
+		isMobileView = window.innerWidth < 840 ? true : false;
+	} else isMobileView = !e.matches;
+
+	//console.log('is mobile?%c ' + isMobileView, cc.bgc.blue);
+	allAddIcons = isMobileView ? allAddIconsMobile : allAddIconsDesktop;
+	setAddIconsAnimation(500);
+}
 
 //==>
 navi.settingsFromOffers();
 setIcons();
-setAddIconsAnimation(1000);
+mediaQuery.addEventListener('change', mobileViewChangeHandler);
+mobileViewChangeHandler(null);
